f5b0f1a0d7e847881e105716280f1d0e003d429c
scarpenter62 <69021762+scarpenter62@users.noreply.github.com>
2020-09-30 16:14:06 -0400

    Added Useful Links Options And generalMenu() function
    Added Useful Links options in the initial and main menu, and added generalMenu() function

--- ui.py
+++ ui.py
@@ -18,6 +18,12 @@ def enterInitialMenu():
         print("3. Find someone you know")
         print("4. Play a video")
         print("5. Quit")
+        print()
+        print("Useful Links:")
+        print("6. General")
+        print("7. Browse InCollege")
+        print("8. Business Solutions")
+        print("9. Directories")
 
         response = input()
         if response == '1':
@@ -30,6 +36,14 @@ def enterInitialMenu():
             print("Video is now playing\n")
         elif response == '5':
             settings.currentState = states.quit
+        elif response == '6':
+            settings.currentState = states.general
+        elif response == '7':
+            settings.currentState = states.browseInCollege
+        elif response == '8':
+            settings.currentState = states.businessSolutions
+        elif response == '9':
+            settings.currentState = states.directories
         else:
             print("Invalid Option, enter the number option you want and press enter")
             continue
@@ -44,6 +58,13 @@ def enterMainMenu():
               "3. Find someone you know\n"
               "4. Learn a new skill\n"
               "5. Logout")
+        print()
+        print("Useful Links:")
+        print("6. General")
+        print("7. Browse InCollege")
+        print("8. Business Solutions")
+        print("9. Directories")
+        
         response = input()
         if response == '1':
             print("Under Construction")
@@ -55,6 +76,14 @@ def enterMainMenu():
             settings.currentState = states.selectSkill
         elif response == '5':
             users.logOutUser()
+        elif response == '6':
+            settings.currentState = states.general
+        elif response == '7':
+            settings.currentState = states.browseInCollege
+        elif response == '8':
+            settings.currentState = states.businessSolutions
+        elif response == '9':
+            settings.currentState = states.directories
         else:
             print("Invalid Option, enter the number option you want and press enter")
             continue
@@ -92,6 +121,37 @@ def enterSkillMenu():
             else:
                 settings.currentState = states.mainMenu
             return False  # Don't learn skill
+        else:
+            print("Invalid Option, enter the number option you want and press enter")
+            continue
+
+            
+def generalMenu():
+    while settings.currentState == states.general:
+        print("Links:")
+        print("1. Sign Up")
+        print("2. Help Center")
+        print("3. About")
+        print("4. Press")
+        print("5. Blog")
+        print("6. Careers")
+        print("7. Developers")
+        result = input()
+        
+        if response == '1':
+            currentState == states.createAccount
+        elif response == '2':
+            print("We're here to help")
+        elif response == '3':
+            print("In College: Welcome to In College, the world's largest college student network with many users in many countries and territories worldwide")
+        elif response == '4':
+            print("In College Pressroom: Stay on top of the latest news, updates, and reports")
+        elif response == '5':
+            print("Under Construction")
+        elif response == '6':
+            print("Under Construction")
+        elif response == '7':
+            print("Under Construction")
         else:
             print("Invalid Option, enter the number option you want and press enter")
             continue
